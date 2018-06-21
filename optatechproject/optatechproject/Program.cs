38b5e902dd51ae66bfeb10e8292bfc6565c89a5a
Jesper Leung <jesperleung23@gmail.com>
2018-06-21 00:38:34 -0400

    cities (loads in from csv of all Canadian cities), provinces loading into Lists
    parse raw address string for address components WIP
    > street #, province, city, postal code can be found

--- optatechproject/optatechproject/Program.cs
+++ optatechproject/optatechproject/Program.cs
@@ -20,6 +20,9 @@ namespace OptaTechProject
 {
     public class OptaTechProject
     {
+        private static List<string> cities;
+        private static List<string> provinces;
+
         //creates SQL connection string and tries to connect to the SQL Server on localhost called SQLEXPRESS
         public static void ConnectToDB()
         {
@@ -94,7 +97,8 @@ namespace OptaTechProject
         {
             try
             {
-                using (TextFieldParser parser = new TextFieldParser(@"..\..\..\..\" + filename))    // getting data file from root folder of project and provided filename
+                // getting data file from root folder of project and provided filename
+                using (TextFieldParser parser = new TextFieldParser(@"..\..\..\..\" + filename))
                 {
                     // telling parser to read a CSV
                     parser.TextFieldType = FieldType.Delimited;
@@ -105,20 +109,142 @@ namespace OptaTechProject
                         string[] fields = parser.ReadFields();
                         foreach (string field in fields)
                         {
-                            // clean up all fields by taking out anything that isn't alphanumeric or a space
-                            string clean = Regex.Replace(field, "[^A-Za-z0-9 ]", "");
-                            Console.WriteLine(clean);
+                            // replace any non-alphanumeric and non-space characters with a space
+                            string clean = Regex.Replace(field, "[^A-Za-z0-9 ]", " ");
+                            ParseAddress(clean);
+                            //Console.WriteLine(clean);
                         }
                     }
                 }
-            }catch (Exception e)
+            }
+            catch (Exception e)
             {
+                // if error, print error to console
                 Console.WriteLine(e.ToString());
             }
+        }
+        // loads list of Canadian cities from csv file into a List and returns it
+        public static List<string> LoadCities()
+        {
+            // initialize List for cities
+            List<string> cities = new List<string>();
+
+            try
+            {
+                // getting data file from root folder of project and provided filename
+                using (TextFieldParser parser = new TextFieldParser(@"..\..\..\..\places.csv"))
+                {
+                    Console.WriteLine("loading cities data...");
+                    // telling parser to read a CSV
+                    parser.TextFieldType = FieldType.Delimited;
+                    parser.SetDelimiters(",");
+                    while (!parser.EndOfData)
+                    {
+                        // read through every line in CSV file
+                        string[] fields = parser.ReadFields();
+                        foreach (string field in fields)
+                        {
+                            // add city name to List
+                            cities.Add(field);
+                        }
+                    }
+                }
+                Console.WriteLine("** done loading cities **");
+            }
+            catch (Exception e)
+            {
+                // if error, print error to console
+                Console.WriteLine(e.ToString());
+            }
+
+            return cities;
+        }
+        // loads list of Canadian provinces into a List and returns it
+        public static List<string> LoadProvinces()
+        {
+            List<string> provinces = new List<string>
+            {
+                "ON",
+                "QC",
+                "BC",
+                "AB",
+                "MB",
+                "NL",
+                "PE",
+                "NS",
+                "NB",
+                "SK",
+                "YT",
+                "NT",
+                "NU"
+            };
+
+            return provinces;
+        }
+        // parses raw string into its parts
+        public static void ParseAddress(string raw)
+        {
+            // initialize counter to be used to ensure all elements are gathered from raw string
+            int counter;
+
+            // split raw string into parts about the spaces
+            string[] split = Regex.Split(raw, @"\s+");
+            // convert array of substrings into List
+            List<string> converted = new List<string>(split);
+
+            // for each element in split string
+            foreach (string s in split)
+            {
+                // reset counter to 0 for every iteration
+                counter = 0;
+                // check if current substring exists in list of provinces
+                if (provinces.Contains(s))
+                {
+                    Console.WriteLine("provinces exists");
+                    counter++;
+                }
+                // check if current substring exists in list of cities
+                else if (cities.Contains(s))
+                {
+                    Console.WriteLine("city exists");
+                    counter++;
+                }
+                // check if current substring is of the postal code format ex. A1A1A1
+                else if (Regex.IsMatch(s, @"\w\d\w\d\w\d"))
+                {
+                    Console.WriteLine("postal code exists");
+                    counter++;
+                }
+                // check if current substring is all numeric (street numbers are all numeric)
+                else if (IsNumeric(s))
+                {
+                    Console.WriteLine("street number exists");
+                    counter++;
+                }
+            }
+        }
+        // iterates through a string to check if each character is a number
+        public static bool IsNumeric(string str)
+        {
+            // get each char c in string str
+            foreach (char c in str)
+            {
+                // if char c is not a number, return false
+                if (c < '0' || c > '9')
+                    return false;
+            }
+            // else return true
+            return true;
         }
 
         public static void Main(string[] args)
         {
+            // loading cities into list for lookup
+            List<string> cities = new List<string>();
+            cities = LoadCities();
+            // loading provinces into list for lookup
+            List<string> provinces = new List<string>();
+            provinces = LoadProvinces();
 
             Console.Write("Please type the filename of the input data file: ");
             string inputfilename = Console.ReadLine();
@@ -126,10 +252,14 @@ namespace OptaTechProject
 
             LoadCSV(inputfilename);
 
-            ConnectToDB();  // connect to database
+            // connect to database
+            //ConnectToDB();
 
             Console.WriteLine("All done. Press any key to finish...");
             Console.ReadKey(true);
+
+
+
         }
     }
 }
