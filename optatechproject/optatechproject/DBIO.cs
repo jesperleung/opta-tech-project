0a7017b00d1490580e2b668e03374741a3d0f177
Jesper Leung <jesperleung23@gmail.com>
2018-06-22 17:29:02 -0400

    changing city + street name algorithm again, going to use combination of street suffix and street number
    modularized all the methods into different classes

---
+++ optatechproject/optatechproject/DBIO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptaTechProject
+{
+    class DBIO
+    {
+        //creates SQL connection string and tries to connect to the SQL Server on localhost called SQLEXPRESS
+        public static void ConnectToDB()
+        {
+            try
+            {
+                // connection string from Server Explorer in VS after adding database
+                string conString = "Data Source=.\\SQLEXPRESS;Initial Catalog=optatechproject;Integrated Security=True";
+                SqlConnection con = new SqlConnection(conString);
+
+                Console.Write("Connecting to SQL Server ... ");
+
+                con.Open();
+                Console.WriteLine("Done.");
+            }
+            catch (SqlException e)
+            {
+                // if error, print error to console
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+    }
+}
---
+++ optatechproject/optatechproject/Utils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptaTechProject
+{
+    class Utils
+    {
+        // iterates through a string to check if each character is a number
+        public static bool IsNumeric(string str)
+        {
+            // get each char c in string str
+            foreach (char c in str)
+            {
+                // if char c is not a number, return false
+                if (c < '0' || c > '9')
+                    return false;
+            }
+            // else return true
+            return true;
+        }
+    }
+}
